b7ffbe07ebe0b67dc31eb1975e4d49aa70ec2356
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-11-29 22:15:07 +0200

    added console log everytime a request is made

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 // Import express
-const default_port = 3000
 const express = require("express")
 const app = express()
+
+app.use((req, res, next) =>{
+    console.log(`new request from ${req.ip}: method:${req.method}, endpoint:'${req.url}' . `)
+    next();
+});
 
 // Configure public folder to be default static object folder.
 app.use(express.static('./public'))
 
 // Setup server to listen on this port
+const default_port = 3000
 app.listen(default_port, () =>
     console.log(`Server listening on port http://localhost:${default_port}/`)
 )
