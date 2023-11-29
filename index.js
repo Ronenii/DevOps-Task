894d71ddaa985e96c1a6f718a04ad21c37ce3c51
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-11-29 19:26:13 +0200

    Remove health endpoint

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ const app = express()
 // Configure public folder to be default static object folder.
 app.use(express.static('./public'))
 
-// Server health check route
-app.get('/health',function(req, res){
-    res.status(200).send("OK")
-})
-
-
 // Setup server to listen on this port
 app.listen(default_port, () =>
     console.log(`Server listening on port http://localhost:${default_port}/`)
